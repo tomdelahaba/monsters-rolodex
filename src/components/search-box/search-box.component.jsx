c0bf9954adf2703016b95a8eaa6d5d3d215c1abd
TJ <tomas.jarolimek@TJ-MacBook-Pro.local>
2021-10-17 23:11:10 +0200

    Monster rolodex project, overall functionality added

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./search-box.style.css";
+
+export const SearchBox = (props) => {
+  return (
+    <input
+      className="search"
+      type="search"
+      placeholder={props.placeholder}
+      onChange={props.handleChange}
+    />
+  );
+};
